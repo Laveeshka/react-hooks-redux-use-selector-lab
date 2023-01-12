b292f7a0bd99474653049314a891928fa0d9a56d
LaveeshkaMahadea <laveeshka94@gmail.com>
2023-01-12 22:52:58 +1100

    use useSelector hook from redux

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,13 +1,17 @@
 import React from "react";
 // add any needed imports here
+import { useSelector } from "react-redux";
 
 function Users() {
+  let users = useSelector((state) => state.users)
   return (
     <div>
       <ul>
         Users!
         {/* Write code here that displays the usernames of all users in the Redux store */}
+        {users.map(user => <li>{user.username}</li>)}
         {/* In addition, display the total number of users curently in the store */}
+        Total Users: {users.length}
       </ul>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import usersReducer from "./features/users/usersSlice";
 
 // add imports and code
+let store = createStore(usersReducer);
 
 ReactDOM.render(
   // add imports and code
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   // add imports and code
   document.getElementById("root")
 );
